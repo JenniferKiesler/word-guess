393b6c6b8713be2acfd56c6fa67f8b2f4df8382e
JenniferKiesler <111549041+JenniferKiesler@users.noreply.github.com>
2022-09-19 21:25:11 -0500

    startRound, gets random word, and renders _ into DOM

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,29 @@
-// select elements from the DOM
-    // start button
-    // word-blanks div
+var startBtn = document.querySelector(".start-button")
+var wordBlanksEl = document.querySelector(".word-blanks")
 
-// array of words
+var words = ["javascript", "variable", "function", "object", "python", "localstorage", "timeout", "interval"]
+var word
+// score variable
+// timeLeft variable
 
-// addEventListener to start button (click)
+function renderCharacters() {
+    var str = ""   
+    for (var i = 0; i < word.length; i++) {
+        // if we have guessed the character
+            // add the character into str
+        // else 
+            // add an _ into str
+        str += "_ "
+    }
+    wordBlanksEl.textContent = str.trim()
+}
+
+function startRound() {
     // get random word from words array
-    // reset the word-blanks, an _ for each letter of the new word
+    var randomIndex = Math.floor(Math.random() * words.length)
+    word = words[randomIndex]
+    renderCharacters()
+}
+
+startBtn.addEventListener("click", startRound)
+    
