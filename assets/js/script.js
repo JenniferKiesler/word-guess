ea0a1d9635348f35481d68d7053bfdb51ce11057
JenniferKiesler <111549041+JenniferKiesler@users.noreply.github.com>
2022-09-19 22:01:12 -0500

    check word, restart round

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,23 @@
 var startBtn = document.querySelector(".start-button")
 var wordBlanksEl = document.querySelector(".word-blanks")
+var scoreEl = document.querySelector(".score")
 
 var validChars = "abcdefghijklmnopqrstuvwxyz"
 var words = ["javascript", "variable", "function", "object", "python", "localstorage", "timeout", "interval"]
 var word
 var guessedCharacters = []
-// score variable
+var score = 0
+
 // timeLeft variable
+
+function checkWord() {
+    var joinWord = wordBlanksEl.textContent.split(" ").join("")
+    if (word === joinWord) {
+        score++
+        scoreEl.textContent = score
+        startRound()
+    }
+}
 
 function handleKeydown(event) {
     if (validChars.includes(event.key)) {
@@ -26,9 +37,11 @@ function renderCharacters() {
         }
     }
     wordBlanksEl.textContent = str.trim()
+    checkWord()
 }
 
 function startRound() {
+    guessedCharacters = []
     var randomIndex = Math.floor(Math.random() * words.length)
     word = words[randomIndex]
     renderCharacters()
