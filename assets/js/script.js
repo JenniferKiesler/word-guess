9ab3f8d2c3774043afe2698b3acf1d80761e23bd
JenniferKiesler <111549041+JenniferKiesler@users.noreply.github.com>
2022-09-19 20:54:23 -0500

    adds pseudocode to js

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+// select elements from the DOM
+    // start button
+    // word-blanks div
+
+// array of words
+
+// addEventListener to start button (click)
+    // get random word from words array
+    // reset the word-blanks, an _ for each letter of the new word
